18a0a107d12314da3f0587ea354b744e9e0c570b
FFF2832 <90552202+FFF2832@users.noreply.github.com>
2021-10-12 12:07:07 +0800

    first commit

---
+++ w4-1.ino
@@ -0,0 +1,23 @@
+#include <Servo.h>
+Servo myservo;
+const int servoPin=14;
+int pos=0;
+
+void setup() {
+  pinMode(13,INPUT);
+ myservo.attach(servoPin);//
+ Serial.begin(115200);
+
+}
+
+// the loop function runs over and over again forever
+void loop() {
+  int input=analogRead(13);
+   int pos = map(input, 0, 4095, 0, 180); 
+ myservo.write(pos);
+ Serial.println(pos);
+  Serial.println(input);
+
+ delay(50);// wait for a second
+
+}
